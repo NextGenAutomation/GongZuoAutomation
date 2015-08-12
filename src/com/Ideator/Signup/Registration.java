c99d335d4eab71b82bb7cf27efbed1be905f78f8
Ramandeep Singh <romy21romy@hotmail.com>
2015-08-12 12:11:49 -0700

    updated version
    just try

--- src/com/Ideator/Signup/Registration.java
+++ src/com/Ideator/Signup/Registration.java
@@ -24,6 +24,43 @@ private static WebElement element = null;
 		element = driver.findElement(By.xpath("html/body/form/div[3]/div/div[1]/div[2]/ul/li[2]/ul/li[2]/a"));
 		return element;
   }
-  
-
+  		public static WebElement idFirstName(WebDriver driver){
+  			WebElement firstName = driver.findElement(By.id("txtFirstName"));
+		return firstName;
+  }
+  		public static WebElement idLastName(WebDriver driver){
+  			WebElement firstName = driver.findElement(By.id("txtLastName"));
+		return firstName;
+  }
+  		public static WebElement idEmail(WebDriver driver){
+  			WebElement firstName = driver.findElement(By.id("txtEmail"));
+		return firstName;
+  }
+  		public static WebElement idPassword(WebDriver driver){
+  			WebElement firstName = driver.findElement(By.id("txtPassword"));
+		return firstName;
+  }
+  		public static WebElement idConfPassword(WebDriver driver){
+  			WebElement firstName = driver.findElement(By.id("txtConfirmPassword"));
+		return firstName;
+  }
+  		public static String idCaptcha(WebDriver driver){
+  			String firstName = driver.findElement(By.xpath(".//*[@id='ContentPlaceHolder1_lblCaptcha']")).getText();
+		return firstName;
+  }
+  		public static WebElement idCapValidate(WebDriver driver){
+  			WebElement firstName = driver.findElement(By.id("txtCaptchaValidate"));
+		return firstName;
+  }
+  		public static WebElement idTerms(WebDriver driver){
+  			WebElement firstName = driver.findElement(By.xpath(".//*[@id='chkAcceptTerms']/div[1]/div"));
+		return firstName;
+  }
+  		public static WebElement idSignup(WebDriver driver){
+  			WebElement firstName = driver.findElement(By.id("btnSignUp"));
+		return firstName;
+  }
+  	
+  		
+  		
 }
